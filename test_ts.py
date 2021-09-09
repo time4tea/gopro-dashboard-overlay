3408f1996c32bb68af9a69d5e16d40168032d497
James Richardson <james@time4tea.net>
2021-09-09 11:51:18 +0100

    hacking together a new timeseries class

--- test_ts.py
+++ test_ts.py
@@ -1,5 +1,6 @@
 import bisect
 import datetime
+from datetime import timedelta
 
 
 class Timeseries:
@@ -27,13 +28,7 @@ class Timeseries:
             greater_idx = bisect.bisect_left(self.dates, dt)
             lesser_idx = greater_idx - 1
 
-            greater_date = self.dates[greater_idx]
-            lesser_date = self.dates[lesser_idx]
-
-            range = (greater_date - lesser_date) / datetime.timedelta(milliseconds=1)
-            point = (dt - lesser_date) / datetime.timedelta(milliseconds=1)
-
-            return self.entries[greater_date] if point / range >= 0.5 else self.entries[lesser_date]
+            return self.entries[self.dates[lesser_idx]].interpolate(self.entries[self.dates[greater_idx]], dt)
 
     def items(self):
         return [self.entries[k] for k in self.dates]
@@ -48,10 +43,57 @@ class Entry:
     def __getattr__(self, item):
         return self.items.get(item, None)
 
+    def interpolate(self, other, dt):
+        if self.dt == other.dt:
+            raise ValueError("Cannot interpolate between equal points")
+        if self.dt > other.dt:
+            raise ValueError("Lower point must be first")
+        if dt < self.dt:
+            raise ValueError("Wanted point before lower")
+        if dt > other.dt:
+            raise ValueError("Wanted point after other")
+
+        if dt == self.dt:
+            return self
+        elif dt == other.dt:
+            return other
+
+        range = (other.dt - self.dt) / timedelta(milliseconds=1)
+        point = (dt - self.dt) / timedelta(milliseconds=1)
+
+        position = point / range
+
+        items = {}
+        for key in self.items.keys():
+            start = self.items[key]
+            end = other.items[key]
+            diff = end - start
+            interp = start + (position * diff)
+            items[key] = interp
+
+        return Entry(dt, **items)
+
+
+def test_interpolating_entry():
+    dt1 = datetime.datetime.fromtimestamp(1631178710)
+    dt2 = dt1 + timedelta(seconds=1)
+
+    point = dt1 + ((dt2 - dt1) / 2)
+
+    e1 = Entry(dt1, lat=1.0, lon=10)
+    e2 = Entry(dt2, lat=2.0, lon=20)
+
+    assert e1.interpolate(e2, dt1).lat == 1.0
+    assert e1.interpolate(e2, dt1).lon == 10.0
+    assert e1.interpolate(e2, dt2).lat == 2.0
+    assert e1.interpolate(e2, dt2).lon == 20.0
+    assert e1.interpolate(e2, point).lat == 1.5
+    assert e1.interpolate(e2, point).lon == 15
+
 
 def test_putting_in_a_point_gets_back_that_point():
-    dt = datetime.datetime.now()
-    dt1 = dt + datetime.timedelta(seconds=1)
+    dt = datetime.datetime.fromtimestamp(1631178710)
+    dt1 = dt + timedelta(seconds=1)
     ts = Timeseries()
     ts.add(Entry(dt, lat=1.0, lon=1.0, alt=12))
     ts.add(Entry(dt1, lat=2.0, lon=1.0, alt=12))
@@ -65,7 +107,7 @@ def test_iterating_empty_timeseries():
 
 def test_adding_multiple_items():
     dt1 = datetime.datetime.fromtimestamp(1631178710)
-    dt2 = dt1 + datetime.timedelta(seconds=1)
+    dt2 = dt1 + timedelta(seconds=1)
     ts = Timeseries()
     ts.add(
         Entry(dt1, lat=3.0, lon=1.0, alt=12),
@@ -77,8 +119,8 @@ def test_adding_multiple_items():
 
 def test_iterates_in_datetime_order():
     dt1 = datetime.datetime.fromtimestamp(1631178710)
-    dt2 = dt1 + datetime.timedelta(seconds=2)
-    dt3 = dt1 + datetime.timedelta(seconds=4)
+    dt2 = dt1 + timedelta(seconds=2)
+    dt3 = dt1 + timedelta(seconds=4)
     ts = Timeseries()
     ts.add(Entry(dt3, lat=3.0, lon=1.0, alt=12))
     ts.add(Entry(dt2, lat=2.0, lon=1.0, alt=12))
@@ -96,7 +138,7 @@ def test_getting_point_before_start_throws():
     dt1 = datetime.datetime.fromtimestamp(1631178710)
     ts.add(Entry(dt1, lat=1.0, lon=1.0, alt=12))
     with pytest.raises(ValueError):
-        ts.get(dt1 - datetime.timedelta(seconds=1))
+        ts.get(dt1 - timedelta(seconds=1))
 
 
 def test_getting_point_after_end_throws():
@@ -105,13 +147,13 @@ def test_getting_point_after_end_throws():
     dt1 = datetime.datetime.fromtimestamp(1631178710)
     ts.add(Entry(dt1, lat=1.0, lon=1.0, alt=12))
     with pytest.raises(ValueError):
-        ts.get(dt1 + datetime.timedelta(seconds=1))
+        ts.get(dt1 + timedelta(seconds=1))
 
 
-def test_getting_intermediate_point_gets_closest():
+def test_getting_intermediate_point_gets_interpolated():
     dt1 = datetime.datetime.fromtimestamp(1631178710)
-    dt2 = dt1 + datetime.timedelta(seconds=1)
-    dt3 = dt1 + datetime.timedelta(seconds=2)
+    dt2 = dt1 + timedelta(seconds=1)
+    dt3 = dt1 + timedelta(seconds=2)
     ts = Timeseries()
     ts.add(Entry(dt1, lat=1.0, lon=1.0, alt=12))
     ts.add(Entry(dt2, lat=2.0, lon=1.0, alt=12))
@@ -120,24 +162,24 @@ def test_getting_intermediate_point_gets_closest():
     assert ts.get(dt1).lat == 1.0
     assert ts.get(dt2).lat == 2.0
     assert ts.get(dt3).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.0)).lat == 1.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.1)).lat == 1.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.2)).lat == 1.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.3)).lat == 1.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.4)).lat == 1.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.5)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.6)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.7)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.8)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=0.9)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.0)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.1)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.2)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.3)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.4)).lat == 2.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.5)).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.6)).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.7)).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.8)).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=1.9)).lat == 3.0
-    assert ts.get(dt1 + datetime.timedelta(seconds=2.0)).lat == 3.0
+    assert ts.get(dt1 + timedelta(seconds=0.0)).lat == 1.0
+    assert ts.get(dt1 + timedelta(seconds=0.1)).lat == 1.1
+    assert ts.get(dt1 + timedelta(seconds=0.2)).lat == 1.2
+    assert ts.get(dt1 + timedelta(seconds=0.3)).lat == 1.3
+    assert ts.get(dt1 + timedelta(seconds=0.4)).lat == 1.4
+    assert ts.get(dt1 + timedelta(seconds=0.5)).lat == 1.5
+    assert ts.get(dt1 + timedelta(seconds=0.6)).lat == 1.6
+    assert ts.get(dt1 + timedelta(seconds=0.7)).lat == 1.7
+    assert ts.get(dt1 + timedelta(seconds=0.8)).lat == 1.8
+    assert ts.get(dt1 + timedelta(seconds=0.9)).lat == 1.9
+    assert ts.get(dt1 + timedelta(seconds=1.0)).lat == 2.0
+    assert ts.get(dt1 + timedelta(seconds=1.1)).lat == 2.1
+    assert ts.get(dt1 + timedelta(seconds=1.2)).lat == 2.2
+    assert ts.get(dt1 + timedelta(seconds=1.3)).lat == 2.3
+    assert ts.get(dt1 + timedelta(seconds=1.4)).lat == 2.4
+    assert ts.get(dt1 + timedelta(seconds=1.5)).lat == 2.5
+    assert ts.get(dt1 + timedelta(seconds=1.6)).lat == 2.6
+    assert ts.get(dt1 + timedelta(seconds=1.7)).lat == 2.7
+    assert ts.get(dt1 + timedelta(seconds=1.8)).lat == 2.8
+    assert ts.get(dt1 + timedelta(seconds=1.9)).lat == 2.9
+    assert ts.get(dt1 + timedelta(seconds=2.0)).lat == 3.0
