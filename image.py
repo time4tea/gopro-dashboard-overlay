b6c0be8b657854f1f22f79ffd89795b566639357
James Richardson <james@time4tea.net>
2021-09-10 09:18:08 +0100

    fix box top

--- image.py
+++ image.py
@@ -76,8 +76,10 @@ class Map:
 
             crop = map_image.crop(bounds)
 
-            ImageDraw.Draw(crop).line((0, 0, 0, desired - 1, desired - 1, desired - 1, desired - 1, 0),
-                                      fill=(255, 255, 255))
+            ImageDraw.Draw(crop).line(
+                (0, 0, 1, desired - 1, desired - 1, desired - 1, desired - 1, 0, 0, 0),
+                fill=(0, 0, 0)
+            )
 
             image.paste(crop, self.location)
 
