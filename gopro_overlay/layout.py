3b110e4ff5ce2c6e4c260e810aa27d5cacf5aaf0
James Richardson <james@time4tea.net>
2022-01-28 09:07:20 +0000

    simplify layout creation

--- gopro_overlay/layout.py
+++ gopro_overlay/layout.py
@@ -1,4 +1,3 @@
-import itertools
 from datetime import timedelta
 
 from PIL import ImageFont
@@ -7,29 +6,29 @@ from .point import Coordinate
 from .privacy import NoPrivacyZone
 from .timeseries import Window
 from .units import units
-from .widgets import Text, date, time, Scene, CachingText
+from .widgets import Text, date, time, Scene, CachingText, Composite
 from .widgets_chart import SimpleChart
 from .widgets_info import BigMetric, ComparativeEnergy, IconPanel
 from .widgets_map import MovingMap, JourneyMap
 
 
 def date_and_time(at, entry, font_title, font_metric):
-    return [
+    return Composite(
         CachingText(at + Coordinate(0, 0), date(lambda: entry().dt), font_title, align="right"),
         Text(at + Coordinate(0, 24), time(lambda: entry().dt), font_metric, align="right"),
-    ]
+    )
 
 
 def gps_info(at, entry, font_title):
-    return [
+    return Composite(
         CachingText(at + Coordinate(0, 0), lambda: "GPS INFO", font_title, align="right"),
         Text(at + Coordinate(-130, 24), lambda: f"Lat: {entry().point.lat:0.6f}", font_title, align="right"),
         Text(at + Coordinate(0, 24), lambda: f"Lon: {entry().point.lon:0.6f}", font_title, align="right"),
-    ]
+    )
 
 
 def maps(entry, renderer, timeseries, privacy_zone):
-    return [
+    return Composite(
         MovingMap(
             at=Coordinate(1900 - 256, 100),
             location=lambda: entry().point,
@@ -44,18 +43,18 @@ def maps(entry, renderer, timeseries, privacy_zone):
             renderer=renderer,
             privacy_zone=privacy_zone
         ),
-    ]
+    )
 
 
 def big_mph(at, entry, font_title, ):
-    return [
+    return Composite(
         BigMetric(
             at,
             lambda: "MPH",
             lambda: f"{entry().speed.to('MPH').magnitude:.0f}" if entry().speed else "-",
             font_title
         )
-    ]
+    )
 
 
 def heartbeat(at, entry, font_metric, font_title):
@@ -123,31 +122,56 @@ def speed_awareness_layout(renderer, font: ImageFont):
         font_title = font.font_variant(size=16)
         font_metric = font.font_variant(size=32)
 
-        return list(itertools.chain(
+        return [
             date_and_time(Coordinate(260, 30), entry, font_title, font_metric),
             gps_info(Coordinate(1900, 36), entry, font_title),
             big_mph(Coordinate(16, 800), entry, font_title),
-            [
-                MovingMap(
-                    size=384,
-                    at=Coordinate(1900 - 384, 100),
-                    location=lambda: entry().point,
-                    azimuth=lambda: entry().azi,
-                    renderer=renderer,
-                    zoom=16
-                ),
-                ComparativeEnergy(Coordinate(450, 850),
-                                  font=font_title,
-                                  speed=lambda: entry().speed,
-                                  person=units.Quantity(84, units.kg),
-                                  bike=units.Quantity(12, units.kg),
-                                  car=units.Quantity(2000, units.kg),
-                                  van=units.Quantity(3500, units.kg)
-                                  )
+            MovingMap(
+                size=384,
+                at=Coordinate(1900 - 384, 100),
+                location=lambda: entry().point,
+                azimuth=lambda: entry().azi,
+                renderer=renderer,
+                zoom=16
+            ),
+            ComparativeEnergy(Coordinate(450, 850),
+                              font=font_title,
+                              speed=lambda: entry().speed,
+                              person=units.Quantity(84, units.kg),
+                              bike=units.Quantity(12, units.kg),
+                              car=units.Quantity(2000, units.kg),
+                              van=units.Quantity(3500, units.kg)
+                              )
+        ]
 
-            ]
-        )
-        )
+    return create
+
+
+def standard_layout(renderer, timeseries, font, privacy=NoPrivacyZone()):
+    def create(entry):
+        font_title = font.font_variant(size=16)
+        font_metric = font.font_variant(size=32)
+
+        window = Window(timeseries, duration=timedelta(minutes=5), samples=256,
+                        key=lambda e: e.alt, fmt=lambda v: v.to("meter").magnitude)
+
+        return [
+            date_and_time(Coordinate(260, 30), entry, font_title, font_metric),
+            gps_info(Coordinate(1900, 36), entry, font_title),
+            maps(entry, renderer, timeseries, privacy),
+            big_mph(Coordinate(16, 800), entry, font_title),
+            altitude(Coordinate(16, 980), entry, font_metric, font_title),
+            gradient(Coordinate(220, 980), entry, font_metric, font_title),
+            SimpleChart(
+                at=Coordinate(400, 980),
+                value=lambda: window.view(entry().dt),
+                font=font_title,
+                filled=True
+            ),
+            temperature(Coordinate(1900, 820), entry, font_metric, font_title),
+            cadence(Coordinate(1900, 900), entry, font_metric, font_title),
+            heartbeat(Coordinate(1900, 980), entry, font_metric, font_title),
+        ]
 
     return create
 
@@ -165,34 +189,3 @@ class Overlay:
     def draw(self, dt):
         self._entry = self.timeseries.get(dt)
         return self.scene.draw()
-
-
-def standard_layout(renderer, timeseries, font, privacy=NoPrivacyZone()):
-    def create(entry):
-        font_title = font.font_variant(size=16)
-        font_metric = font.font_variant(size=32)
-
-        window = Window(timeseries, duration=timedelta(minutes=5), samples=256,
-                        key=lambda e: e.alt, fmt=lambda v: v.to("meter").magnitude)
-
-        return list(itertools.chain(
-            date_and_time(Coordinate(260, 30), entry, font_title, font_metric),
-            gps_info(Coordinate(1900, 36), entry, font_title),
-            maps(entry, renderer, timeseries, privacy),
-            big_mph(Coordinate(16, 800), entry, font_title),
-            [
-                altitude(Coordinate(16, 980), entry, font_metric, font_title),
-                gradient(Coordinate(220, 980), entry, font_metric, font_title),
-                SimpleChart(
-                    at=Coordinate(400, 980),
-                    value=lambda: window.view(entry().dt),
-                    font=font_title,
-                    filled=True
-                ),
-                temperature(Coordinate(1900, 820), entry, font_metric, font_title),
-                cadence(Coordinate(1900, 900), entry, font_metric, font_title),
-                heartbeat(Coordinate(1900, 980), entry, font_metric, font_title),
-            ])
-        )
-
-    return create
