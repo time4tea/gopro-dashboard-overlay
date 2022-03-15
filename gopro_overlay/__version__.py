66a39846d32c6bbba8937ac606a13c9fbf6efbee
James Richardson <james@time4tea.net>
2022-03-15 12:31:25 +0000

    bump version, update readme

--- gopro_overlay/__version__.py
+++ gopro_overlay/__version__.py
@@ -2,7 +2,7 @@
 __title__ = 'gopro_overlay'
 __description__ = 'Dashboards for GoPro'
 __url__ = 'https://github.com/time4tea/gopro-dashboard-overlay'
-__version__ = '0.32.0'
+__version__ = '0.33.0'
 __author__ = 'James Richardson'
 __author_email__ = 'james@time4tea.net'
 __license__ = 'MIT'
