90cb337edbba72202a2406240ee011abeb7bda1b
James Richardson <james@time4tea.net>
2022-07-14 13:43:34 +0100

    bump version, update readme

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name="gopro-overlay",
-    version="0.46.0",
+    version="0.47.0",
     description="Overlay graphics dashboards onto GoPro footage",
     long_description=README,
     long_description_content_type="text/markdown",
