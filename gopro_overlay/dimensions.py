1f66abf917923c55ddeaa20cf6647e4b6e7eeb3a
James Richardson <james@time4tea.net>
2022-09-24 21:21:04 +0100

    remove use of namedtuple

--- gopro_overlay/dimensions.py
+++ gopro_overlay/dimensions.py
@@ -1,10 +1,14 @@
-from collections import namedtuple
+from dataclasses import dataclass
 
-Dimension = namedtuple("Dimension", ["x", "y"])
+
+@dataclass(frozen=True)
+class Dimension:
+    x: int
+    y: int
 
 
 def dimension_from(s):
     components = list(map(int, s.split("x")))
     if len(components) != 2:
         raise ValueError("dimension should be in format XxY")
-    return Dimension(components[0], components[1])
+    return Dimension(x=components[0], y=components[1])
