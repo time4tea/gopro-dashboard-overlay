e3d83ff4a76fa5d4c34bc9ae5dc5cf1ba760f8a6
James Richardson <james@time4tea.net>
2022-12-08 16:39:33 +0000

    license info for widgets

--- tests/test_widgets_cairo.py
+++ tests/test_widgets_cairo.py
@@ -62,6 +62,12 @@ class CairoCircuit:
         image.alpha_composite(to_pillow(self.surface), (0, 0))
 
 
+#
+# This is a conversion of the ADA INDUSTRIAL CONTROL WIDGET LIBRARY
+# http://www.dmitry-kazakov.de/ada/aicwl.htm
+# Any bugs are my own
+# The original is GPLv2 - I don't know how this applies yet.
+
 @dataclasses.dataclass(frozen=True)
 class EllipseParameters:
     centre: Coordinate
