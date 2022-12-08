08d4fcac643a633f43520971bbe793144e3f5bbb
James Richardson <james@time4tea.net>
2022-12-08 16:41:43 +0000

    license info for widgets

--- tests/test_widgets_cairo.py
+++ tests/test_widgets_cairo.py
@@ -67,6 +67,7 @@ class CairoCircuit:
 # http://www.dmitry-kazakov.de/ada/aicwl.htm
 # Any bugs are my own
 # The original is GPLv2 - I don't know how this applies yet.
+# The original has link-time exception...so.. ?
 
 @dataclasses.dataclass(frozen=True)
 class EllipseParameters:
