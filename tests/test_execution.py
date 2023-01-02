f5f8f0ccc2e2bf2e55a90ebed722a4999920e24b
James Richardson <james@time4tea.net>
2023-01-02 18:47:15 +0000

    remove threaded execution - remove test too..

--- tests/test_execution.py
+++ tests/test_execution.py
@@ -2,7 +2,7 @@ import contextlib
 from pathlib import Path
 
 from gopro_overlay.common import temp_file_name
-from gopro_overlay.execution import InProcessExecution, ThreadingExecution
+from gopro_overlay.execution import InProcessExecution
 
 
 @contextlib.contextmanager
@@ -13,15 +13,6 @@ def do_execute(execution, cmd):
 def test_in_process_execution():
     filename = temp_file_name()
     execution = InProcessExecution(redirect=filename)
-    with do_execute(execution, ["cat"]) as out:
-        out.write("Hello".encode())
-
-    assert Path(filename).read_text() == "Hello"
-
-
-def test_threading_execution():
-    filename = temp_file_name()
-    execution = ThreadingExecution(redirect=filename)
     with do_execute(execution, ["cat"]) as out:
         out.write("Hello".encode())
 
