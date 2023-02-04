f08fc2f86c78001b6980934c75bc6c522025023e
James Richardson <james@time4tea.net>
2023-02-04 11:06:35 +0000

    add some more tests, and approved images

--- tests/test_widgets_gradient_bar.py
+++ tests/test_widgets_gradient_bar.py
@@ -2,6 +2,7 @@ import pytest
 
 from gopro_overlay.dimensions import Dimension
 from gopro_overlay.widgets.gradient_bar import GradientBar
+from gopro_overlay.widgets.widgets import Widget
 from tests import test_widgets_setup
 from tests.approval import approve_image
 from tests.test_widgets import time_rendering
@@ -10,18 +11,26 @@ font = test_widgets_setup.font
 ts = test_widgets_setup.ts
 
 
+def do_bar(bar: Widget):
+    return time_rendering(name="test_power_zones", dimensions=Dimension(500, 75), widgets=[bar])
+
+
 @pytest.mark.gfx
 @approve_image
-def test_gauge():
-    return time_rendering(
-        name="test_power_zones",
-        dimensions=Dimension(500, 75),
-        widgets=[
-            GradientBar(
-            size=Dimension(400, 30),
-            reading=lambda: 250,            
-        )
-        ]
-    )
+def test_gauge_over_max():
+    return do_bar(GradientBar(size=Dimension(400, 30), max_value=400, reading=lambda: 500,))
 
+@pytest.mark.gfx
+@approve_image
+def test_gauge_at_max():
+    return do_bar(GradientBar(size=Dimension(400, 30), max_value=400, reading=lambda: 400,))
 
+@pytest.mark.gfx
+@approve_image
+def test_gauge_lower():
+    return do_bar(GradientBar(size=Dimension(400, 30), max_value=400, reading=lambda: 100,))
+
+@pytest.mark.gfx
+@approve_image
+def test_gauge_med():
+    return do_bar(GradientBar(size=Dimension(400, 50), cr=30, max_value=300, reading=lambda: 200,))
